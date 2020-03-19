ec6efb713d2fb91fb1fc64ce3a37783b9334466c
Wagner Lopes <waalopes99@gmail.com>
2020-03-19 11:45:10 -0300

    initial app structure

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Generate random room name if needed
+if (!location.hash) {
+  location.hash = Math.floor(Math.random() * 0xffffff).toString(16);
+}
+const roomHash = location.hash.substring(1);
+
+const configuration = {
+  iceServers: [
+    {
+      urls: "stun:stun.l.google.com:19302" // Google's public STUN server
+    }
+  ]
+};
+
+function onSuccess() {}
+function onError(error) {
+  console.error(error);
+}
